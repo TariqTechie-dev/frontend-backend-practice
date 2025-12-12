de9f6f805e966b1e4d899f24ebad61331f2b7cb8
TariqTechie-dev <tariqtech843@gmail.com>
2025-12-12 10:13:17 +0500

    add views and index for EJS template engine

---
+++ backend/EJSDir/index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+let port=3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req,res)=>{
+    res.render("home");
+})
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
+
