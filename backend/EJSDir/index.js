2b238686f1984046752f5e54c2df912b08689dc7
TariqTechie-dev <tariqtech843@gmail.com>
2025-12-14 21:12:59 +0500

    add views folder and set path.jion in index.js

--- backend/EJSDir/index.js
+++ backend/EJSDir/index.js
@@ -1,13 +1,18 @@
-const express=require('express');
-const app=express();
-let port=3000;
-
+const express = require('express');
+const app = express();
+let port = 3000;
+const path = require("path")
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, "/views"));
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.render("home.ejs");
 })
-app.listen(port,()=>{
+app.get("/rolldice", (req, res) => {
+    let diceVal=Math.floor(Math.random()*6) +1
+    res.render("rolldice.ejs", {num:diceVal});
+})
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
