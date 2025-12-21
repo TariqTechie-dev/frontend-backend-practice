5d85b433b5c158bfa22b7ec21971508b0b43b7fc
TariqTechie-dev <tariqtech843@gmail.com>
2025-12-21 11:24:31 +0500

    add error.ejs ,change in index and insta

--- backend/EJSDir/index.js
+++ backend/EJSDir/index.js
@@ -13,17 +13,33 @@ app.get("/", (req, res) => {
 app.get("/rolldice", (req, res) => {
     let diceVal = Math.floor(Math.random() * 6) + 1;
     let followers = ["john_doe", "jane_smith", "cool_user", "tech_guru", "nature_lover"];
-  
+
     res.render("rolldice.ejs", { num: diceVal, diceVal, followers });
 })
 
+// app.get("/ig/:username", (req, res) => {
+//     let { username } = req.params;
+//     let followers = ["john_doe", "jane_smith", "cool_user", "tech_guru", "nature_lover"]; // 👈 yahan add karo
+
+//     res.render("instagram.ejs", { username, followers }); // 👈 followers bhi pass karo
+// })
+
 app.get("/ig/:username", (req, res) => {
     let { username } = req.params;
-    let followers = ["john_doe", "jane_smith", "cool_user", "tech_guru", "nature_lover"]; // 👈 yahan add karo
+
+    const instaData = require("./data.json");
+    let userData = instaData[username];
+    console.log(userData);
+   if (userData) {
+       res.render("instagram.ejs", { userData });
+   } else {
+    res.render("error.ejs");
+   }
     
-    res.render("instagram.ejs", { username, followers }); // 👈 followers bhi pass karo
-})
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
